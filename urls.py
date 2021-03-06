dc990ad7142f31f0871fda5bbba82301b006fa31
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-06 05:03:54 -0800

    update

--- urls.py
+++ urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def tivan(request):
+    return HttpResponse('tivan')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tivan/', tivan),
 ]
